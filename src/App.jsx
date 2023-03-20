ac8af1f75216c1fc277638a4bca625cafafcbe0f
keltsjsx <kelts.jsx@gmail.com>
2023-03-20 18:47:28 +0700

    useState dan prevState

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-import Card from './components/Card';
+import { useState } from 'react';
+import Content from './components/Content';
 import Button from './components/Button';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((prevState) => prevState + 1);
+  };
+
   return (
-    <div className={'bg-slate-100 text-slate-800 tracking-tight antialiased flex items-center justify-center min-h-screen'}>
-      <div className={'max-w-xl w-full'}>
-        <Card>
-          <Card.Title>Hello React!</Card.Title>
-          <Card.Body>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi atque eius numquam obcaecati quia modi, rerum architecto. Enim eos tenetur
-            fuga, placeat ratione, earum numquam maiores libero, amet quisquam doloremque?
-          </Card.Body>
-          <Card.Footer>
-            <Button>Register</Button>
-          </Card.Footer>
-        </Card>
+    <Content>
+      <h1 className={'text-5xl font-bold'}>{count}</h1>
+      <div className={'mt-5 flex items-center gap-2'}>
+        <Button onClick={handleClick}>+1</Button>
+        <Button
+          onClick={() => {
+            handleClick();
+            handleClick();
+            handleClick();
+          }}>
+          +3
+        </Button>
       </div>
-    </div>
+    </Content>
   );
 };
 
